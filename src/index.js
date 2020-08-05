2a490737b1ab1566bab93771d33a4e352407aa98
datboi34 <edgar87250@gmail.com>
2020-08-05 18:56:15 -0400

    need to practice for loops besides that keep practicing javascript this assignment called on a array and used createElement() and adding classes using purely javascript

--- src/index.js
+++ src/index.js
@@ -1,32 +1,48 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  // your code here
+  const todoList = document.createElement("ul");
+  todoList.classList.add("todo-list");
+  document.body.appendChild(todoList);
+  // loop
+  for (let i = 0; i < todos.length; i++) {
+    let list = todos[i];
+    let accomplishList = `
+       <li class = "lets-do_this">
+       <input type="checkbox"  id = "salon" / >
+       <label for = "salon" > ${list.description}</label
+        
+       
+       </li>
+      `;
 
-    h1.innerText = "Todo List"
+    todoList.innerHTML += accomplishList;
+  }
 
-    app.appendChild(h1)
-    // and maybe some here
+  h1.innerText = "Todo List";
+
+  app.appendChild(h1);
+  // and maybe some here
 }
 
-renderTodoApp()
+renderTodoApp();
